e0ee99d85140d9d297d618e38c78466ce9ec197b
Yu Ricky <leoleo5994@gmail.com>
2022-01-08 19:14:15 +0800

    interactive-comments-app init

---
+++ src/components/CommentList.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>我是list</main>
+</template>
+
+<script>
+export default {
+  name: "CommentList",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/components/_comment-list.scss";
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
 
-createApp(App).use(store).mount('#app')
+import "@/assets/scss/main.scss";
+
+createApp(App).use(store).mount("#app");
